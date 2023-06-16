215845f0448d18815db35cdb94630d35cb4894ef
kravmaxim@gmail.com <Cegthgfhjkm!666>
2023-06-16 19:48:55 +0300

    -

--- src/test/java/org/max/lesson3/seminar/ConvertAmountsTest.java
+++ src/test/java/org/max/lesson3/seminar/ConvertAmountsTest.java
@@ -1,6 +1,34 @@
 package org.max.lesson3.seminar;
 
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.max.lesson3.AbstractTest;
 
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+
 public class ConvertAmountsTest extends AbstractTest {
+
+    @Test
+    void getConvertAmounts_whenValid_shouldReturn() {
+        ConvertAmountsDto response = given()
+                .queryParam("apiKey", getApiKey())
+                .queryParam("ingredientName", "flour")
+                .queryParam("sourceAmount", 2.5)
+                .queryParam("sourceUnit", "cups")
+                .queryParam("targetUnit", "grams")
+                .when()
+                .get(getBaseUrl()+"recipes/convert")
+                .then()
+                .statusCode(200)
+                .time(Matchers.lessThan(1000l))
+                .extract()
+                .response()
+                .body().as(ConvertAmountsDto.class);
+
+        Assertions.assertEquals(response.getTargetAmount(),312.5);
+
+    }
 }
